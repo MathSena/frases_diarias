c472910e68126586b8fd881826734984902d19fa
Matheus Sena <MathSena07@hotmail.com>
2022-09-24 13:04:26 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// Função principal
+void main(){
+
+  // runApp
+  runApp(MaterialApp(
+    title: "Frases do dia",
+    home: Container(color: Colors.amberAccent,) // Estrutura inicial do projeto
+  ));
+
+}
